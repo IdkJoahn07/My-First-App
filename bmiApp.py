716658cf2016c8122eea11d0cb421a8ded7cbc28
409-21-Nichkhun​ <54412@yupparaj.ac.th>
2026-08-11 14:27:13 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -14,7 +14,7 @@ if st.button("คำนวณค่า BMI"):
     height_m - height_cm / 100
     bmi = weight / (height_m ** 2)
 
-    st.write("---"
+    st.write("---")
     st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}##")
 
     #ส่วนที่ 4 แปลผลค่า BMI ตามเกณฑ์
