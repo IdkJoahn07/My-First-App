1f0ef5e69eb3f7da529f96afe93dde9dfb668f93
409-21-Nichkhun​ <54412@yupparaj.ac.th>
2026-08-24 23:23:19 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -15,7 +15,7 @@ if st.button("คำนวณค่า BMI"):
     bmi = weight / (height_m ** 2)
 
     st.write("---")
-    st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}")
+    st.header(f"ค่า BMI ของคุณคือ: {bmi:.2f}")
 
     #ส่วนที่ 4 แปลผลค่า BMI ตามเกณฑ์
     if bmi < 18.5:
